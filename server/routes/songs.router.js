edae90f959b94cec93784eea60aba6aec38eaaab
Danny Mussie <dannymussie7@gmail.com>
2023-01-14 16:42:24 -0600

    new delete and post routes in the songs router

--- server/routes/songs.router.js
+++ server/routes/songs.router.js
@@ -43,10 +43,67 @@ router.get('/recommendations/:recommendations', rejectUnauthenticated, (req, res
   });
 
 /**
- * POST route template
+ * Delete user songs from the front end and database
+ */
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
+    // delete route code here
+    const queryText = 'DELETE FROM "user_song_recs" WHERE "song_id"=$1';
+    console.log('delete req.params.id', req.params.id);
+    pool.query(queryText, [req.params.id])    
+    .then(response => {
+    //promise.all???
+    res.send(response.data)
+    })
+    .catch(error => {
+    // is there a better way to throw an error in this case?
+    console.log(error);
+    })
+  });
+
+
+/**
+ * Insert song into database 
  */
 router.post('/', (req, res) => {
   // POST route code here
-});
+  const insertEventQuery = `
+    INSERT INTO "song_recs" ("spotify_uri", "spotify_id", "spotify_category_id", "spotify_user_id", "spotify_url")
+    VALUES ($1, $2, $3, $4, $5) RETURNING id;`;
+
+    pool.query(insertEventQuery, 
+        [req.body.spotify_uri,
+        req.body.spotify_id,
+        req.body.spotify_category_id,
+        req.body.spotify_user_id,
+        req.body.spotify_url])
+        pool.query(insertEventQuery, 
+            [req.body.displayName,
+            req.body.city,
+            req.body.time,
+            req.body.uri])
+        .then((result) => {
+            console.log('New song Id:', result.rows[0].id);
+            
+            const songId = result.rows[0].id; 
+    
+            // now handle user reference
+            const userSongRecsJunctionQuery = `
+            INSERT INTO "user_song_recs" ("user_id", "song_id")
+            VALUES ($1, $2)`
+            // look up asychronous express request (pizza parlour assignment)
+            // more recommended approach
+            // potential lecture topic!!!
+            pool.query(userSongRecsJunctionQuery, [req.user.id, songId])
+            .then(result => {
+                res.sendStatus(201);
+            }).catch(error => {
+                console.log(error);
+                res.sendStatus(500);
+            })
+    
+        // catch for the first query    
+        }).catch((error) => {
+            console.log('Error in POST', error);
+        })});
 
 module.exports = router;
